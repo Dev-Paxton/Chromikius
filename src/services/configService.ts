5f0f283199f4fd3ff31dfe6eb8bbe10c6c36a3c8
Justin <justindroegemeier@gmail.com>
2022-01-03 23:21:50 +0100

    feat: configService which allow to handle dev and prod configs

---
+++ src/services/configService.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv"
+import config from "config"
+
+
+interface DbSettings {
+    host: string
+    databaseName: string
+    userName: string
+    password: string
+}
+
+interface BotSettings {
+    id: string
+    token: string 
+}
+
+interface Config {
+    database : DbSettings
+    bot: BotSettings
+}
+
+export default class ConfigService {
+    private static config : Config
+
+    public static getConfig() {
+        if(!this.config) this.config = this.loadConfig();
+
+        return this.config;
+    }
+
+    private static loadConfig() : Config {
+        dotenv.config()
+        const database = { ...config.get("database"), password: process.env.DATABASE_PSWD} as DbSettings;        
+        const bot = {...config.get("bot"), token: process.env.BOT_TOKEN} as BotSettings; 
+
+        console.log(process.env.NODE_ENV);
+        console.log(database);
+
+        return {
+            database,
+            bot
+        };
+    }
+}
