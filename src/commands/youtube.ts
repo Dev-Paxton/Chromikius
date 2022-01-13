56cfb048741982d915e152606cc52c2675304eb5
Goro <benedikt.wutzke@gmail.com>
2022-01-13 21:14:50 +0100

    Added youtube command

---
+++ src/commands/youtube.ts
@@ -0,0 +1,14 @@
+import { MessageEmbed } from "discord.js";
+import { Command } from "../structures/Command";
+
+export default new Command({
+    name: "youtube",
+    description: "Kevin Chromiks YouTube Kanal",
+    execute: async ({ interaction }) => {
+        const embed = new MessageEmbed()
+            .setColor("#ff9e00")
+            .setTitle("Kevin Chromik's YouTube Kanal:\nhttps://www.youtube.com/c/KevinChromik")
+
+        await interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+})
