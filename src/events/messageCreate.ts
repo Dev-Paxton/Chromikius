952a3ffb2d0ff2936b496455bcc1e5438f013be9
Goro <benedikt.wutzke@gmail.com>
2022-01-16 10:38:53 +0100

    Added reaction on survey messages

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,18 @@
 import { Message } from "discord.js";
 import Database from "../utils/Database";
 import { Event } from "../structures/Event";
+import Config from "../utils/Config";
 
 export default new Event("messageCreate", async (message: Message) => {
     if (message.author.bot) return
 
-    await Database.levelsystem_register(message.author.id)
-    await Database.levelsystem_add_xp(message.author.id)
-    await Database.levelsystem_level_up(message.author.id, message)
+    if (Config.database.required) {
+        await Database.levelsystem_register(message.author.id)
+        await Database.levelsystem_add_xp(message.author.id)
+        await Database.levelsystem_level_up(message.author.id, message)
+    }
+
+    if (message.channelId === Config.guild.surveyChannelId) {
+        message.react("<:Ja:724324262768279592>").then(() => message.react("<:Nein:724325055923879989>"))
+    }
 })
